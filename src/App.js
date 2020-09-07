92a648e233b1d26e8030a373e21f62c8adeee51a
Vikash Arya <vikasarya457@gmail.com>
2020-09-07 17:21:44 +0530

    uncontrolled-forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import Main from './Components/MainComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
+  render(){
+    return(
+      <div className='App'>
+        <BrowserRouter>
+          <Main/>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
