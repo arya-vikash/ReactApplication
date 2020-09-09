4efd62207e3de305e0912836dd8ddc808f3645f4
Vikash Arya <vikasarya457@gmail.com>
2020-09-09 19:32:35 +0530

    React actions e.g.- loading and failed

--- src/Components/DishdetailComponent.js
+++ src/Components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React ,{ Component} from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle, Breadcrumb, BreadcrumbItem, Button,Col, Row, Label, ModalBody, Modal, ModalHeader } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Errors, Control } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required=(val)=>val && val.length;
 const maxLength =(len)=>(val)=>!val || (val.length<=len);
@@ -23,9 +24,8 @@ class CommentForm extends Component{
     }
     
     handleSubmit(values) {
-        console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
-        // event.preventDefault();
+        this.toggleModal();
+        this.props.addComment(this.props.dishId,values.rating,values.author,values.comment)
     }
 
     render(){
@@ -49,13 +49,13 @@ class CommentForm extends Component{
                                 </Col>
                             </Row>
                             <Row className='form-group'>
-                                <Label htmlFor='name' md={12}>Your Name</Label>
+                                <Label htmlFor='author' md={12}>Your Name</Label>
                                 <Col md={12}>
-                                    <Control.text model='.name' id='name' name='name' className='form-control' placeholder='Your Name'
+                                    <Control.text model='.author' id='author' name='author' className='form-control' placeholder='Your Name'
                                     validators={{
                                         required,minLength: minLength(3),maxLength: maxLength(15)
                                     }}/>
-                                    <Errors className='text-danger' model='.name' show='touched'
+                                    <Errors className='text-danger' model='.author' show='touched'
                                                 messages={{
                                                     required: 'Required',
                                                     minLength: 'Must be greater than 2 characters',
@@ -94,7 +94,7 @@ class CommentForm extends Component{
         );
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments ,dishId,addComment}) {
         if (comments != null) {
             comments = comments.map((comment) => {
                 return (<li key="comment.id">
@@ -111,7 +111,7 @@ class CommentForm extends Component{
                     <ul className="list-unstyled">
                         {comments}
                     </ul>
-                    <CommentForm/>
+                    <CommentForm addComment={addComment} dishId={dishId}/>
                 </div>
             )
             
@@ -124,35 +124,55 @@ class CommentForm extends Component{
     }
 
     const DishDetail=(props)=> {
-        if (props.dish != null) {
-            return (
+        if (props.isLoading) {
+            return(
                 <div className="container">
-                    <div className="row">
-                        <Breadcrumb>
-
-                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
-                        </div>                
-                    </div>
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderDish dish={props.dish} />
-                        </div>
-                        <div className="col-12 col-md-5 m-1">
-                            <RenderComments comments={props.comments} />
-                        </div>
+                    <div className="row">            
+                        <Loading />
                     </div>
                 </div>
             );
         }
-        else
-            return (
-                <div></div>
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
             );
+        }
+        else if (props.dish != null)
+            if (props.dish != null) {
+                return (
+                    <div className="container">
+                        <div className="row">
+                            <Breadcrumb>
+
+                                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                            </Breadcrumb>
+                            <div className="col-12">
+                                <h3>{props.dish.name}</h3>
+                                <hr />
+                            </div>                
+                        </div>
+                        <div className="row">
+                            <div className="col-12 col-md-5 m-1">
+                                <RenderDish dish={props.dish} />
+                            </div>
+                            <div className="col-12 col-md-5 m-1">
+                                <RenderComments comments={props.comments} addComment={props.addComment}
+                                dishId={props.dish.id} />
+                            </div>
+                        </div>
+                    </div>
+                );
+            }
+            else
+                return (
+                    <div></div>
+                );
     }
 
 export default DishDetail;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,22 @@
-import {createStore} from 'redux';
-import { Reducer, initialState } from './reducer'
+import {createStore , combineReducers ,applyMiddleware} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 
 export const ConfigureStore = () => {
     const store = createStore(
-        Reducer, // reducer
-        initialState, // our initialState
+        combineReducers({
+            dishes:Dishes,
+            comments:Comments,
+            leaders:Leaders,
+            promotions:Promotions
+        }),
+        applyMiddleware(thunk,logger)
+        
     );
 
     return store;
